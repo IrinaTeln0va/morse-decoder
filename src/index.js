7e803b5011fb8fc01f19fad40e7b7714ca2cad7c
IrinaTeln0va <ira8365@mail.ru>
2020-03-07 18:55:06 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -37,8 +37,59 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const MORSE_ARR = Object.entries(MORSE_TABLE);
+
 function decode(expr) {
-    // write your solution here
+    const encodedWordsArr = expr.split('**********');
+    const decodedWordsArr = encodedWordsArr.map((encodedWord) => {
+        return decodeWord(encodedWord);
+    });
+    return decodedWordsArr.join(' ');
+}
+
+const decodeWord = (encodedWord) => {
+    const encodedLettersArr = devideWordIntoLetters(encodedWord);
+    const decodedLettersArr = encodedLettersArr.map((encodedLetter) => {
+        for (let i = 0; i < encodedLetter.length; i++) {
+            if (encodedLetter[i] == '0') {
+                i++;
+            } else {
+                return decodeLetter(encodedLetter.slice(i));
+            }
+        }
+    });
+    return decodedLettersArr.join('');
+}
+
+const devideWordIntoLetters = (encodedWord) => {
+    const encodedLettersArr = [];
+    for (let i = 0; i < encodedWord.length; i += 10) {
+        encodedLettersArr.push(encodedWord.slice(i, i + 10));
+    }
+    return encodedLettersArr;
+}
+
+const decodeLetter = (numericCodeStr) => {
+    let numericPairArr = [];
+    for (let i = 0; i < numericCodeStr.length; i+=2) {
+        numericPairArr.push(numericCodeStr.slice(i, i + 2));
+    }
+
+    let morseKey = numericPairArr.map((elem) => {
+        if (elem == '10') {
+            return '.';
+        } else {
+            return '-';
+        }
+    }).join('');
+
+    let decodedLetter;
+    MORSE_ARR.forEach(([key, value]) => {
+        if (key == morseKey) {
+            decodedLetter = value;
+        }
+    });
+    return decodedLetter;
 }
 
 module.exports = {
